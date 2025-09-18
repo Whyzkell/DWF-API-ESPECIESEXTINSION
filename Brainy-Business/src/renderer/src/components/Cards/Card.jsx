e26cc3f7ae5e1bba060f09de033611ac6251f04a
Whyzkell <105887364+Whyzkell@users.noreply.github.com>
2025-09-18 10:10:48 -0600

    nada

--- Brainy-Business/src/renderer/src/components/Cards/Card.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Card({ className = '', children }) {
-  return (
-    <div className={`bg-white rounded-2xl ring-1 ring-neutral-200 ${className}`}>{children}</div>
-  )
-}
